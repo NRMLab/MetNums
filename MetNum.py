86c0e51454134d0915e9767f0b275374b2e858ac
Mariuki <mario_netta12@hotmail.com>
2021-06-10 10:57:51 -0500

    Adding Interpol_Intercal

--- MetNum.py
+++ MetNum.py
@@ -203,34 +203,144 @@ def MN_NRM(funciones, vec, variables):
 
     return allresdf,  tabledxprint, m
 
-def InterPol(x,y,color=['ro','b'], val=0, polyrange=[0,5]):
-        '''Esta función sirve para resolver por medio del método de Newton-Raphson Multivariable.
-       Necesita como entrada las funciones y los valores  inicial X0.
-       Ejemplo de uso:
-       ## Valores a interpolar##
-       x=[930,983,1050,1088,1142,1316,1320]
-       y=[10,20,40,60,100,200,400]
-       ## Función ##
-       InterPol(x,y,val=1250,color=['gd','m--'],polyrange=[0,5])
-       '''
+def InterPol(x,y,color=['ro','b'], val=0, polyrange=[0,5], gen = 0, mgraphs = 0):
+    '''Esta función sirve para realizar un modelo que ajuste a los datos por medio
+    del método de interpolación. Hace el ajuste de acuerdo a la población más densa
+    de datos. Recibe como entrada vectores "x" y "y". Y si es requerido un valor
+    específico a evaluar. Puedes jugar con los parámetros para que se ajuste con distintas
+    métricas de error mediante el parámetro "gen", distintos números de gráficas con mgraphs.
+    Ejemplo de uso:
+    ## Valores a interpolar##
+    x=[930,983,1050,1088,1142,1316,1320]
+    y=[10,20,40,60,100,200,400]
+    ## Función ##
+    InterPol(x,y,color=['kD','c--'], val=1250, polyrange=[1,7], gen = 1, mgraphs = 1)
+    '''
     x=np.array(x)
     y=np.array(y)
     error=[]
     a={}
+
+    if mgraphs:
+      fig, axs = plt.subplots(int(np.floor(polyrange[1]/2)),2, figsize=(15,15))
+      fig.suptitle('Puntos ajustados distintos grados de polinomio')
+
     for i in range(polyrange[0],polyrange[1]):
-        coeficientes = np.polyfit(x, y,i+1)
+        coeficientes = np.polyfit(x, y,i)
         evaluacion = np.polyval(coeficientes,x)
 #         error=((1/len(x))* np.sum(np.subtract(y,evaluacion)**2))
-        error = np.sum(np.subtract(y,evaluacion))
+        if gen == 1:
+          error = np.sum(np.subtract(y,evaluacion))
+        else:
+          error = np.abs(np.sum(np.subtract(y,evaluacion)))
         a[error]=coeficientes
+        if mgraphs:
+          poly = np.poly1d(coeficientes)
+          new_x = np.linspace(x[0], x[-1])
+          new_y = poly(new_x)
+
+          kr = i -1
+          axs[int(np.floor(kr/2)), kr%2].plot(x, y, "kD", new_x, new_y,'c--')
+          axs[int(np.floor(kr/2)), kr%2].set_title('Poliniomio de grado: {}'.format(i))
     mejor = min(a.keys())
     poly = np.poly1d(a[mejor])
-    new_x = np.linspace(x[0], x[-1])
-    new_y = poly(new_x)
-    plt.plot(x, y, color[0], new_x, new_y,color[1])
-    plt.xlim([x[0]-1, x[-1] + 1 ])
+    if not mgraphs:
+      new_x = np.linspace(x[0], x[-1])
+      new_y = poly(new_x)
+      plt.plot(x, y, color[0], new_x, new_y,color[1])
+      # plt.xlim([x[0]-1, x[-1] + 1 ])
     print('Polinómio: \n',poly)
     if val != 0:
         calculado=np.polyval(a[mejor],val)
-        print('\n El valor para {} es {}'.format(val,calculado))
+        print('\n El valor con polinomio de grado {} para {} es {}'.format( len(a[mejor])-1,val,calculado))
     return
+
+def intercalar_mm(xs,y, valor):
+  xs = np.array(xs)
+  y = np.array(y)
+  mayores = xs[xs > valor][:]
+  menores = xs[xs[xs < valor].argsort()][::-1]
+  mayoresy = y[np.where(xs>valor)]
+  menoresy = y[np.where(xs<valor)][::-1]
+  traslp = []
+  traslpy = []
+  i=0
+  while i <= np.abs(len(mayores)-len(menores))+1 :
+    while i < (min(len(menores),len(mayores))):
+      traslp.append(mayores[i])
+      traslp.append(menores[i])
+      traslpy.append(mayoresy[i])
+      traslpy.append(menoresy[i])
+      i += 1
+    if len(mayores) ==  (min(len(menores),len(mayores))):
+      traslp.append(menores[i])
+      traslpy.append(menoresy[i])
+    else:
+      traslp.append(mayores[i])
+      traslpy.append(mayoresy[i])
+    i += 1
+  return traslp, traslpy
+
+def InterPol_Intercal(x,y,color=['ro','b'], val=0, polyrange=[0,5], gen = 0, mgraphs = 0):
+  '''Esta función sirve para realizar un modelo que ajuste a los datos por medio
+  del método de interpolación. Hace el ajuste de acuerdo a la población más densa
+  de datos. Recibe como entrada vectores "x" y "y". Y si es requerido un valor
+  específico a evaluar. Puedes jugar con los parámetros para que se ajuste con distintas
+  métricas de error mediante el parámetro "gen", distintos números de gráficas con mgraphs.
+  Ejemplo de uso:
+  ## Valores a interpolar##
+  x=[930,983,1050,1088,1142,1316,1320]
+  y=[10,20,40,60,100,200,400]
+  ## Función ##
+  InterPol_Intercal(x,y,color=['kD','c--'], val=1250, polyrange=[1,7], gen = 1, mgraphs=0)
+  '''
+  xr,yr = x,y
+  x,y = intercalar_mm(x,y,val)
+  error=[]
+  evaluacion = []
+  a={}
+
+  if mgraphs:
+    fig, axs = plt.subplots(int(np.floor(polyrange[1]/2)),2, figsize=(15,15))
+    fig.suptitle('Puntos ajustados distintos grados de polinomio')
+
+  for k in range(polyrange[0],polyrange[1]):
+    ecs=np.zeros([k+1,k+1])
+    for i in range(k+1):
+        for j in range(k+1):
+            x1_eval=x[i]**j
+            ecs[i,j]=x1_eval
+
+
+    coeficientes=np.linalg.solve(ecs,y[:k+1])
+    polyeval = 0
+    evaluacion = np.polyval(coeficientes[::-1],x[:k+1])
+
+    if gen == 1:
+      error = np.sum(np.subtract(y[:k+1],evaluacion))
+    else:
+      error = np.abs(np.sum(np.subtract(y[:k+1],evaluacion)))
+
+    a[error]=coeficientes
+
+    if mgraphs:
+      poly = np.poly1d(coeficientes[::-1])
+      new_x = np.linspace(x[0], x[-1])
+      new_y = poly(new_x)
+
+      kr = k -1
+      axs[int(np.floor(kr/2)), kr%2].plot(x, y, "kD", new_x, new_y,'c--')
+      axs[int(np.floor(kr/2)), kr%2].set_title('Poliniomio de grado: {}'.format(k))
+
+  mejor = min(a.keys())
+  poly = np.poly1d(a[mejor][::-1])
+  if not mgraphs:
+    # poly = np.poly1d(a[mejor][::-1])
+    new_x = np.linspace(xr[0], xr[-1])
+    new_y = poly(new_x)
+    plt.plot(xr, yr, color[0], new_x, new_y,color[1])
+    # plt.xlim([x[0]-1, x[k] + 1 ])
+  print('Polinómio: \n',poly)
+  if val != 0:
+      calculado=np.polyval(a[mejor][::-1],val)
+      print('\n El valor con polinomio de grado {} para {} es {}'.format( len(a[mejor])-1,val,calculado))
